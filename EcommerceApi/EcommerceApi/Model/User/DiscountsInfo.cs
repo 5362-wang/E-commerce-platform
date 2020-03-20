dd3a64d959ac71adf24d0c70c4fd908cf60d87d3
xiaowuo <59269804+xiaowuo@users.noreply.github.com>
2020-03-20 21:21:01 +0800

    武

---
+++ EcommerceApi/EcommerceApi/Model/User/DiscountsInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    /// <summary>
+    /// 部门表
+    /// </summary>
+    class DiscountsInfo
+    {
+        /// <summary>
+        /// 部门Id
+        /// </summary>
+        public int DiscountsId { get; set; }
+        /// <summary>
+        /// 部门名称
+        /// </summary>
+        public string DiscountsWay { get; set; }
+    }
+}
---
+++ EcommerceApi/EcommerceApi/Model/User/UserTypeInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    class UserTypeInfo
+    {
+        /// <summary>
+        /// 用户状态表
+        /// </summary>
+        class DiscountsInfo
+        {
+            /// <summary>
+            /// 状态Id
+            /// </summary>
+            public int UserTypeId { get; set; }
+            /// <summary>
+            /// 状态名称
+            /// </summary>
+            public string UserTypeName { get; set; }
+        }
+    }
+}
