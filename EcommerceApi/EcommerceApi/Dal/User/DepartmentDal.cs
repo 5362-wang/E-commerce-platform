17a7a80a76250b62940f1acaf32ffe8e0316c0ff
xiaowuo <59269804+xiaowuo@users.noreply.github.com>
2020-03-25 11:49:46 +0800

    武希宇

---
+++ EcommerceApi/EcommerceApi/Dal/User/DepartmentDal.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace Dal.User
+{
+    public class DepartmentDal
+    {
+        DBHelper db = new DBHelper();
+        /// <summary>
+        /// 添加部门
+        /// </summary>
+        /// <returns></returns>
+        public int Add(Department d)
+        {
+            string sql = string.Format("insert into DiscountsInfo values('{0}')", d.DiscountsWay);
+            return db.ExecuteNonQuery(sql);
+        }
+        /// <summary>
+        /// 删除部门
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <returns></returns>
+        public int Del(int Id)
+        {
+            string sql = $"delect from DiscountsInfo where Id = {Id}";
+            return db.ExecuteNonQuery(sql);
+        }
+        /// <summary>
+        /// 修改部门
+        /// </summary>
+        /// <param name="d"></param>
+        /// <returns></returns>
+        public int Upt(Department d)
+        {
+            string sql = string.Format("update DiscountsInfo set DiscountsWay='{0}' where Id='{1}'", d.DiscountsWay, d.DiscountsId);
+            return db.ExecuteNonQuery(sql);
+        }
+        /// <summary>
+        /// 显示部门
+        /// </summary>
+        /// <returns></returns>
+        public List<Department> Get()
+        {
+            string sql = $"select * from DiscountsInfo";
+            return db.GetToList<Department>(sql);
+        }
+    }
+}
---
+++ EcommerceApi/EcommerceApi/Dal/User/UserTypeinfoDal.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace Dal.User
+{
+    public class UserTypeinfoDal
+    {
+        DBHelper db = new DBHelper();
+        /// <summary>
+        /// 添加用户状态
+        /// </summary>
+        /// <returns></returns>
+        public int Add(UserTypeInfo u)
+        {
+            string sql = string.Format("insert into UserTypeInfo values('{0}')", u.UserTypeName);
+            return db.ExecuteNonQuery(sql);
+        }
+        /// <summary>
+        /// 删除用户状态
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <returns></returns>
+        public int Del(int Id)
+        {
+            string sql = $"delect from UserTypeInfo where Id = {Id}";
+            return db.ExecuteNonQuery(sql);
+        }
+        /// <summary>
+        /// 修改
+        /// </summary>
+        /// <param name="d"></param>
+        /// <returns></returns>
+        public int Upt(UserTypeInfo u)
+        {
+            string sql = string.Format("update UserTypeInfo set UserTypeName='{0}' where Id='{1}'", u.UserTypeName,u.UserTypeId);
+            return db.ExecuteNonQuery(sql);
+        }
+        
+    }
+}
