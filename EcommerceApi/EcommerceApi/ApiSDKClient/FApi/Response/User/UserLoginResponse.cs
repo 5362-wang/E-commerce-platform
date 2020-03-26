c6f8db07b1c3a325d3ea76aa6d64b06aa4989a44
王玺凯 <XIXI@>
2020-03-26 11:40:05 +0800

    登录更新

--- EcommerceApi/EcommerceApi/ApiSDKClient/FApi/Response/User/UserLoginResponse.cs
+++ EcommerceApi/EcommerceApi/ApiSDKClient/FApi/Response/User/UserLoginResponse.cs
@@ -17,8 +17,16 @@ namespace ApiSDKClient
         /// </summary>
         public int Uid { get; set; }
         /// <summary>
+        /// 用户名
+        /// </summary>
+        public string UserName { get; set; }
+        /// <summary>
         /// 用户部门
         /// </summary>
         public int DepartmentId { get; set; }
+        /// <summary>
+        /// 用户头像
+        /// </summary>
+        public string UserImg { get; set; }
     }
 }
